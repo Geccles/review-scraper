dcd74f41afca026b74baae45f7e1b417b2a5b1ed
Galina Nosti <geccles01@gmail.com>
2023-02-22 08:19:50 -0500

    initial commit

---
+++ review-scraper.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import json
+
+from app_store_scraper import AppStore
+
+# compass-real-estate-homes/id692766504
+compass = AppStore(country='us', app_name='compass-real-estate-homes', app_id = '692766504')
+
+# amount of reviews we want to save
+compass.review(how_many=2000)
+
+
+compass.reviews
+
+dataFrame = pd.DataFrame(np.array(compass.reviews),columns=['review'])
+
+dataFrameJoined = dataFrame.join(pd.DataFrame(dataFrame.pop('review').tolist()))
+
+dataFrameJoined.head()
+
+dataFrameJoined.to_csv('Compass-app-reviews.csv')
